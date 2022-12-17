9bf12b612a29f02dfb6735ad5badf10c3a38f8e5
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-17 11:05:25 +0100

    basic Model Classes 💖

---
+++ Model/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Model;
+use Core\Model;
+
+class User extends  Model
+{
+    private  string $username;
+    private  string $email;
+    private  string $password;
+
+    /**
+     * @param string $username
+     * @param string $email
+     * @param string $password
+     */
+    public function __construct(string $username, string $email, string $password)
+    {
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+        parent::__construct(get_class($this));
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function toJson(): string{
+        return json_encode($this);
+    }
+
+
+    public function logOut():void{
+        session_unset();
+        session_destroy();
+    }
+    public function login(): bool{
+
+        return false;
+    }
+}
