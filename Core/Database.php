dff0aba0faa29263cd10707245d9a3aa07d61ad4
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-17 11:02:19 +0100

    Database class

---
+++ Core/Database.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Core;
+use PDO;
+
+class Database
+{
+    public $connection;
+    public $statement;
+
+    public function __construct($config, $username = 'root', $password = '')
+    {
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = []): static
+    {
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($params);
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (! $result) {
+            abort();
+        }
+
+        return $result;
+    }
+}
