4cbaafa9485d34fd990bd391900553649ac44479
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-17 11:39:50 +0100

    test functions 🚀

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@ function dd($value)
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
-
     die();
 }
 
@@ -21,6 +20,14 @@ function authorize($condition, $status = Response::FORBIDDEN)
     }
 }
 
+function abort($code = 404) {
+    http_response_code($code);
+
+    require "views/{$code}.php";
+
+    die();
+}
+
 function base_path($path)
 {
     return BASE_PATH . $path;
@@ -31,4 +38,54 @@ function view($path, $attributes = [])
     extract($attributes);
 
     require base_path('views/' . $path);
+}
+ function hashPassword($value): string
+{
+    return password_hash($value, PASSWORD_DEFAULT);
+}
+
+function verifyPassword($value, $hash): bool
+{
+    return password_verify($value, $hash);
+}
+
+
+function imageUpload($imageRequest, $imagePath = "assets/images/"): string
+{
+    global $msgError;
+    $imagename  = rand(1000, 10000) . $_FILES[$imageRequest]['name'];
+    $imagetmp   = $_FILES[$imageRequest]['tmp_name'];
+    $imagesize  = $_FILES[$imageRequest]['size'];
+    $allowExt   = array("jpg", "png", "gif", "mp3", "pdf");
+    $strToArray = explode(".", $imagename);
+    $ext        = end($strToArray);
+    $ext        = strtolower($ext);
+    
+    if (!empty($imagename) && !in_array($ext, $allowExt)) {
+        $msgError = "EXT";
+    }
+    if ($imagesize > 2 * MB) {
+        $msgError = "size";
+    }
+    if (empty($msgError)) {
+        move_uploaded_file($imagetmp,  $imagePath . $imagename);
+        return $imagename;
+    } else {
+        return "fail";
+    }
+}
+
+
+function get($value)
+{
+    if (isset($_GET[$value])) {
+        return $_GET[$value];
+    }
+}
+
+function post($value)
+{
+    if (isset($_POST[$value])) {
+        return $_POST[$value];
+    }
 }
