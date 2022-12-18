08d777c33596d8af8b3412b3cca8c60902cfb98e
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-18 12:45:05 +0100

    add base font Poppins to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./**/*.{html,js,ts,jsx,tsx,php}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-1' : "#F5F7FF",
+        'color-2' : "#0074BE",
+        'color-3' : "#0156FF",
+        'color-4' : "#01A4FF",
+        'color-5' : "#A2A6B0",
+        'color-6' : "#CACDD8",
+        'color-7' : "#000000",
+        'color-8' : "#C94D3F",
+        'color-9' : "#78A962",
+        'color-10' : "#666666",
+  
+      },
+      fontFamily: {
+        'poppins': ['"Poppins"', 'sans-serif'],
+      }
+    },
+
   },
   plugins: [],
 }
