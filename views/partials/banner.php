9b850868570439627a1be208e020ea9e3dc595f4
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-18 20:32:57 +0100

    banner

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,15 +1,16 @@
-<div class="banner  bg-color-7 w-full text-color-6 p-2 hidden lg:block">
+<div class="banner  bg-color-7 w-full text-color-6 p-2 ">
     <section class="container flex m-auto justify-around ">
     <div class="date-info flex ">
         <p class="font-bold"><span class="text-color-5">Mon-Thu:</span> 9:00 AM - 5:30 PM</p>
         <i class="pl-2 fa-solid fa-chevron-right "></i>
     </div>
-    <div class="content-info">
+    <div class="content-info hidden lg:block">
         <p class="text-color-6 ">
             Visit our showroom in 1234 Street Adress City Address, 1234 <span class="text-white font-bold underline cursor-pointer"><a href="/">Contact Us </a></span>
         </p>
     </div>
-    <div class="contact-info flex gap-2">
+    <div class="hidden lg:block">
+    <div class="contact-info flex gap-2 ">
         <p class="text-white font-semibold">
             Call Us: (00) 1234 5678
         </p>
@@ -17,6 +18,7 @@
             <a href="#"><i class="fab fa-facebook-f"></i></a>
             <a href="#"><i class="fab fa-instagram"></i></a>
         </div>
+    </div>
     </div>
     </section>
 </div>
