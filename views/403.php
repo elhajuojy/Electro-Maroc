480219b2102af3622ce957c024d71259e7537833
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-18 20:31:48 +0100

    404 and 403 redesign pages

--- views/403.php
+++ views/403.php
@@ -1,1 +1,8 @@
-403
+<?php view('partials/header.php', ['title' => 'Page Not Found', 'description' => 'This is the 404 Page']); ?>
+<body class="bg-blue-700">
+    <div class="flex flex-col justify-center items-center h-screen text-white gap-2">
+        <h1 class="text-9xl font-bold">403</h1>
+        <h2 class="text-3xl font-bold">Page Not autherzied </h2>
+        <a href="/" class="text-2xl font-bold underline">Go Back</a>
+    </div>
+</body>
--- views/404.php
+++ views/404.php
@@ -1,1 +1,9 @@
-404
+<?php view('partials/header.php', ['title' => 'Page Not Found', 'description' => 'This is the 404 Page']); ?>
+<section class="w-full h-full bg-color-8">
+<div class="flex flex-col justify-center items-center h-screen text-white gap-2">
+    <h1 class="text-9xl font-bold">404</h1>
+    <h2 class="text-3xl font-bold">Page Not Found</h2>
+    <a href="/" class="text-2xl font-bold underline">Go Back</a>
+</div>
+</section>
+<?php view ('partials/footer.php'); ?>
