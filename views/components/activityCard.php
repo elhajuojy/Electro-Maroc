10cfe2be29b52cec89688b6d7be68d4df9d4ba74
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-20 23:56:57 +0100

    fix little change on ux

--- views/components/activityCard.php
+++ views/components/activityCard.php
@@ -8,7 +8,7 @@
         </div>
     </div>
     <div class="card-price">
-        <h3 class="text-2xl font-bold"><?=$value?></h3>
+        <h3 class="text-2xl font-bold opacity-80 mt-2"><?=$value?></h3>
     </div>
     <div class="card-grow">
         <span class="opacity-70 font-semibold"> <?=$grow?> </span><span class="text-green-500"><i class="fa-solid fa-arrow-up"></i></span>
